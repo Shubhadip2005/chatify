8a533990677d1b739310f284e8aaef7c8883973f
Shubhadip Das <shubhadipdas733@gmail.com>
2025-10-26 19:17:55 +0530

    signup implemented on the frontend

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -13,7 +13,7 @@ export const generateToken = (userId, res) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,// prevent XSS attacks: cross-site scripting
-        secure: process.env.NODE_ENV === 'development',
+        secure: ENV.NODE_ENV === "development" ? false : true,
         sameSite: 'strict', // CSRF protection: cross-site request forgery
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -13,19 +14,21 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());//req.body parser
+app.use(cors({origin: ENV.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if(ENV.NODE_ENV === 'development'){
+if(ENV.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
+console.log(ENV.NODE_ENV)
 app.listen(PORT, () =>{
   console.log('Server running on port:' + PORT)
   connectDB();
