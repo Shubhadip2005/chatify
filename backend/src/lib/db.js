f9e490546aad92df658f4583a4463a4881e69e57
Shubhadip Das <shubhadipdas733@gmail.com>
2025-10-27 06:42:37 +0530

    development ready

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
-import { ENV } from './env.js'
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
     const { MONGO_URI } = ENV;
-    if (!MONGO_URI) throw new Error('MONGO_URI is not defined in environment variables');
-    
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
     const conn = await mongoose.connect(ENV.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); //1 indicates failure, 0 indicates success
+    console.error("Error connection to MONGODB:", error);
+    process.exit(1); // 1 status code means fail, 0 means success
   }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import { signup, login, logout, updateProfile } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
-import { arcjetProtection } from '../middleware/arcjet.middleware.js';
+import express from "express";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
@@ -13,6 +13,6 @@ router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, (req, res) => res.status(200).json({ message: "Authorized", user: req.user   }));
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
