d5fbf13ad957bdefef7023ec19f425892c298456
Shubhadip Das <shubhadipdas733@gmail.com>
2025-10-30 19:14:41 +0530

    for render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chatify-m2kjq.sevalla.app",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chatify-n6m6.onrender.com/api",
   withCredentials: true,
 });
